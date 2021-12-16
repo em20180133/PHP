0f66e6d3ce9ea8fe905a39950344f12a0ed8f0b2
em20180133 <enamarkovic91@gmail.com>
2021-12-16 16:27:45 +0100

    Napravljen je css za index stranicu

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-   
+    <link rel="stylesheet" type="text/css" href="dizajn/index.css">
     <title>Dobrovoljno volontiranje</title>
 </head>
 <body>
-    <h2>Dobro nam došli volonteri!</h2><br><br>
+    <div class="vdiv">
+    <h2>Dobro nam došli volonteri!</h2><br>
     <p>Svake godine, zbog hladnoće, gladi, bolesti izazvanih istima, ostanemo bez hiljade i hiljade najmlađih stanovnika naše planete. <br>
 Hvala vam što ste pružate svoju pomoć prilikom pripreme i realizacije humanitarnih koncerata, koji se organizuju u cilju prikupljanja <br>
 novčanih sredstava onima kojima su ta sredstva najviše potrebna. Hvala!</p>
     <div class="forma_za_prijavu">
-        <div class="div1">
+        <div class="divf">
             <form method="POST" action="#">
                 <div class="prijava">
                 <label class="username">Korisničko ime: </label> <br>
@@ -22,16 +23,18 @@ novčanih sredstava onima kojima su ta sredstva najviše potrebna. Hvala!</p>
                     <br>
                     <label for="password">Šifra: </label><br>
                     <input type="password" name="password" class="fpassword" required><br> <br>
-                    <button type="submit" class="btn_prijavi_se" name="submit">Prijavi se</button>
+                    <button type="submit" class="btn_prijavi_se" name="submit">PRIJAVI SE</button>
                 </div>
             </form><br>
             
         </div>
         <img src="slike/deca.jpg" alt="Hvala vam" id="deca"><br> <br> <br>
-<a href="https://www.youtube.com/c/SpecialBooksbySpecialKids" id=snimak>-> -> -> WATCH <- <- <-</a>
+<a href="https://www.youtube.com/c/SpecialBooksbySpecialKids" id=snimak> <span> -> -> -> WATCH <- <- <- </span></a> <br> <br>
+    </div>
     </div>
 
 
+    
 
 </body>
 </html> 
