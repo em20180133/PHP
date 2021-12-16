17fb7c6d7e301f211b2bd8fcb9bf2fbafd735faf
em20180133 <enamarkovic91@gmail.com>
2021-12-16 15:04:28 +0100

    Azuriran index.html-kostur

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>Dobrovoljno volontiranje</title>
+</head>
+<body>
+    <h2>Dobro nam došli volonteri!</h2><br><br>
+    <p>Svake godine, zbog hladnoće, gladi, bolesti izazvanih istima, ostanemo bez hiljade i hiljade najmlađih stanovnika naše planete. <br>
+Hvala vam što ste pružate svoju pomoć prilikom pripreme i realizacije humanitarnih koncerata, koji se organizuju u cilju prikupljanja <br>
+novčanih sredstava onima kojima su ta sredstva najviše potrebna. Hvala!</p>
+    <div class="forma_za_prijavu">
+        <div class="div1">
+            <form method="POST" action="#">
+                <div class="prijava">
+                <label class="username">Korisničko ime: </label> <br>
+                    <input type="text" name="username" class="fusername"  required> <br>
+                    <br>
+                    <label class="email">Email: </label> <br>
+                    <input type="text" name="email" class="femail"  required> <br>
+                    <br>
+                    <label for="password">Šifra: </label><br>
+                    <input type="password" name="password" class="fpassword" required><br> <br>
+                    <button type="submit" class="btn_prijavi_se" name="submit">Prijavi se</button>
+                </div>
+            </form><br>
+            
+        </div>
+        <img src="slike/deca.jpg" alt="Hvala vam" id="deca"><br> <br> <br>
+<a href="https://www.youtube.com/c/SpecialBooksbySpecialKids" id=snimak>-> -> -> WATCH <- <- <-</a>
+    </div>
+
+
+
+</body>
+</html> 
+
+        
+    </div>
+</body>
+</html>
